b824bc0d8657c9e268e9cdfc692ad6f9e659ed8a
Wilamis Micael de Araujo Aviz <wmaa@ic.ufal.br>
2022-11-23 12:42:30 -0300

    Use css module

--- Alura/react-ts/alura-studies/src/components/Button/index.tsx
+++ Alura/react-ts/alura-studies/src/components/Button/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./Button.module.scss";
 
 class Button extends React.Component {
   render() {
-    return <button>Botao</button>;
+    return <button className={style.botao}>Botao</button>;
   }
 }
 
---
+++ Alura/react-ts/alura-studies/src/components/Form/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from "../Button";
+import style from "./Form.module.scss";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
+          <label htmlFor="task">Add a new study</label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            placeholder="What do you wanna study?"
+            required
+          />
+        </div>
+        <div className={style.inputContainer}>
+          <label htmlFor="time">Time</label>
+          <input
+            type="time"
+            step="1"
+            name="time"
+            id="time"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
+        </div>
+        <Button />
+      </form>
+    );
+  }
+}
+export default Form;
