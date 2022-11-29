eff0ad678aaef06acfabba44faf4d19b62436338
wilamisaviz15 <wmaa@ic.ufal.br>
2022-11-28 23:08:20 -0300

    Create a balance card

--- Projects/my-expenses/src/App.tsx
+++ Projects/my-expenses/src/App.tsx
@@ -3,14 +3,16 @@ import CardBalance from "./components/Card/CardBalance";
 import Sidebar from "./components/Sidebar";
 import styles from "./App.module.scss";
 import SimpleCard from "./components/Card/SimpleCard";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <main className={styles.container}>
       <Sidebar />
       <div className={styles.container__content}>
-        <SimpleCard />
-        {/* <CardBalance /> */}
+        {/* <SimpleCard />
+        <CardBalance /> */}
+        <Dashboard />
       </div>
     </main>
   );
