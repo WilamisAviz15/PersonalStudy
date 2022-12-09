134b413d5f48ee61f979982b3d98ff48ac0c8455
wilamisaviz15 <wmaa@ic.ufal.br>
2022-12-08 22:41:45 -0300

    Fix color from color selector on DialogWallet

--- Projects/my-expenses/src/components/Dialog/DialogWallet/index.tsx
+++ Projects/my-expenses/src/components/Dialog/DialogWallet/index.tsx
@@ -3,11 +3,18 @@ import styles from "./DialogWallet.module.scss";
 import checkedIcon from "../../../assets/img/checked-icon.svg";
 import InputText from "../../Input/InputText";
 import InputNumber from "../../Input/InputNumber";
+import { useState } from "react";
 
 const DialogWallet = () => {
+  const [checked, setChecked] = useState(true);
   const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(event);
+  };
+
+  const handleInputChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.checked = !checked;
+    setChecked(!checked);
   };
 
   return (
@@ -20,26 +27,17 @@ const DialogWallet = () => {
         <label htmlFor="">Initial Value:</label>
         <InputNumber />
         <label htmlFor="">Color:</label>
-        {/* <div>
-          <label htmlFor="radio-1"></label>
-          <input
-            type="radio"
-            className={styles.radio}
-            id="radio-1"
-            name="group"
-          />
 
-          <button className={styles.content__colors__dot}></button>
-          <button className={styles.content__colors__dot}></button>
-          <button className={styles.content__colors__dot}></button>
-          <button className={styles.content__colors__dot}></button>
-          <button className={styles.content__colors__dot}></button> 
-        </div> */}
-        //TODO:
-        {/* https://www.sliderrevolution.com/resources/styling-radio-buttons/ */}
         <div className={styles.content__custom__radios}>
           <div>
-            <input type="radio" id="color-1" name="color" value="color-1" />
+            <input
+              type="radio"
+              id={styles["color-1"]}
+              name="color"
+              value="color-1"
+              checked={checked}
+              onChange={(event) => handleInputChecked(event)}
+            />
             <label htmlFor="color-1">
               <span>
                 <img src={checkedIcon} alt="Checked Icon" />
@@ -48,7 +46,12 @@ const DialogWallet = () => {
           </div>
 
           <div>
-            <input type="radio" id="color-2" name="color" value="color-2" />
+            <input
+              type="radio"
+              id={styles["color-2"]}
+              name="color"
+              value="color-2"
+            />
             <label htmlFor="color-2">
               <span>
                 <img src={checkedIcon} alt="Checked Icon" />
@@ -57,7 +60,12 @@ const DialogWallet = () => {
           </div>
 
           <div>
-            <input type="radio" id="color-3" name="color" value="color-3" />
+            <input
+              type="radio"
+              id={styles["color-3"]}
+              name="color"
+              value="color-3"
+            />
             <label htmlFor="color-3">
               <span>
                 <img src={checkedIcon} alt="Checked Icon" />
@@ -66,7 +74,12 @@ const DialogWallet = () => {
           </div>
 
           <div>
-            <input type="radio" id="color-4" name="color" value="color-4" />
+            <input
+              type="radio"
+              id={styles["color-4"]}
+              name="color"
+              value="color-4"
+            />
             <label htmlFor="color-4">
               <span>
                 <img src={checkedIcon} alt="Checked Icon" />
