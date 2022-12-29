dfc50da5ae1e127fa9c65cc64238f9633e0791f1
Wilamis Micael de Araujo Aviz <wmaa@ic.ufal.br>
2022-12-29 10:29:31 -0300

    feat: adicionando footer e rota 404

--- Alura/react-ts/aluroni/src/routes.js
+++ Alura/react-ts/aluroni/src/routes.js
@@ -5,10 +5,12 @@ import Menu from "pages/Menu";
 import Header from "components/Header";
 import DefaultPage from "components/DefaultPage";
 import About from "pages/About";
+import Footer from "components/Footer";
+import NotFound from "pages/NotFound";
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className="container">
       <BrowserRouter>
         <Header />
         <Routes>
@@ -17,7 +19,9 @@ export default function AppRouter() {
             <Route path="cardapio" element={<Menu />} />
             <Route path="sobre" element={<About />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </main>
   );
