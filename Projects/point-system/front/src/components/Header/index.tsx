53c93e02aee70dd5650f5731ed91199635b805a7
Wilamis Micael de Araujo Aviz <wmaa@ic.ufal.br>
2023-04-08 22:12:24 -0300

    quick start project

---
+++ Projects/point-system/front/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="" alt="" />
+      <ul>
+        <li>Inicio</li>
+        <li>Marcações</li>
+        <li>Perfil</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
