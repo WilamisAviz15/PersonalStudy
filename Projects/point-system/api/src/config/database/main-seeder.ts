bdfff983cd08ba75a0655d2b0710c686e5e32919
Wilamis Micael de Araujo Aviz <wmaa@ic.ufal.br>
2023-04-11 17:49:39 -0300

    feat: create migration and seed from privileges

--- Projects/point-system/api/src/config/database/main-seeder.ts
+++ Projects/point-system/api/src/config/database/main-seeder.ts
@@ -5,11 +5,12 @@ import { MenuGroupsSeed } from './seeds/menu-groups.seed';
 import { MenusSeed } from './seeds/menus.seeds';
 import { RolesSeed } from './seeds/roles.seed';
 import { ActionsMenuSeed } from './seeds/action-menus.seed';
+import { PrivilegesSeed } from './seeds/privileges.seed';
 
 export class MainSeeder implements Seeder {
   public async run(dataSource: DataSource): Promise<any> {
     await runSeeders(dataSource, {
-      seeds: [ActionsSeed, MenuGroupsSeed, MenusSeed, RolesSeed, ActionsMenuSeed],
+      seeds: [ActionsSeed, MenuGroupsSeed, MenusSeed, RolesSeed, ActionsMenuSeed, PrivilegesSeed],
     });
   }
 }
