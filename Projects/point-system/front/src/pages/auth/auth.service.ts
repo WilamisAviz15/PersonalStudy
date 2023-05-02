9d61a0621ceffedbae13ca377162098b5ef33a12
Wilamis Aviz <wmaa@ic.ufal.br>
2023-05-02 18:08:36 -0300

    feat: change service

--- Projects/point-system/front/src/pages/auth/auth.service.ts
+++ Projects/point-system/front/src/pages/auth/auth.service.ts
@@ -1,18 +1,29 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 import { SignInInterface } from "./interfaces/signIn.interface";
 import { UserInterface } from "./interfaces/user.interface";
 import callAxios from "../../shared/components/useAxios";
 
-const authService = {
+class AuthService {
+  constructor() {}
+
   async authenticate(data: SignInInterface) {
-    return await callAxios<SignInInterface, UserInterface & { accessToken: string }>("auth/login", axios.post, data);
-  },
+    const jwt = await callAxios<SignInInterface, UserInterface & { accessToken: string }>(
+      "auth/login",
+      axios.post,
+      data
+    );
+    this.saveUser(jwt.data.accessToken);
+    return jwt;
+  }
+
+  saveUser(accessToken: string) {}
 
   handleCurrentUser(user: UserInterface, accessToken: string) {
     localStorage.setItem("access_token", accessToken);
     localStorage.setItem("user", JSON.stringify(user));
-  },
-};
-
+  }
+}
+const authService = new AuthService();
+Object.freeze(authService);
 export default authService;
